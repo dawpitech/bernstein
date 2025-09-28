78f54b812d5786c4cc68ee5a1a91f6aae27d859b
Antoine <me@antoinebellanger.fr>
2025-09-28 21:44:17 +0200

    fix: apply.sh

--- apply.sh
+++ apply.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env sh
+kubectl apply -f cadvisor.daemonset.yaml
 kubectl apply -f postgres.secret.yaml \
     -f postgres.configmap.yaml \
     -f postgres.volume.yaml \
@@ -7,6 +8,19 @@ kubectl apply -f postgres.secret.yaml \
 kubectl apply -f redis.configmap.yaml \
     -f redis.deployment.yaml \
     -f redis.service.yaml
+kubectl apply -f poll.deployment.yaml \
+    -f worker.deployment.yaml \
+    -f result.deployment.yaml \
+    -f poll.service.yaml \
+    -f result.service.yaml \
+    -f poll.ingress.yaml \
+    -f result.ingress.yaml
 kubectl apply -f traefik.rbac.yaml \
     -f traefik.deployment.yaml \
     -f traefik.service.yaml
+
+kubectl wait --for=condition=ready pod -l app=postgres --timeout=120s
+
+echo "CREATE TABLE IF NOT EXISTS votes \
+(id text PRIMARY KEY, vote text NOT NULL);" \
+    | kubectl exec -i $(kubectl get pod -l app=postgres -o jsonpath='{.items[0].metadata.name}') -c postgres -- psql -U user -d db
