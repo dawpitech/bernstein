95a95db2712aad0a763c72c5114fa624ff037dd5
Antoine <me@antoinebellanger.fr>
2025-09-28 20:52:56 +0200

    fix: traefik

---
+++ reset.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env sh
+
+kubectl delete deployment --all
+kubectl delete deployment -n kube-public --all
+kubectl delete service --all
+kubectl delete pod --all
+kubectl delete ingress --all
+kubectl delete persistentvolumeclaims postgres-pvc
